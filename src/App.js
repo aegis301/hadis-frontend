2ca84e06090e8dd10265d0d6fa3ceaaa41d4d71f
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-10-08 17:09:31 +0200

    add app bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import PatientsList from "./components/PatientsList";
+import HadisAppBar from "./components/AppBar";
 
 function App() {
 	return (
 		<div className="App">
 			<React.StrictMode>
+				<HadisAppBar />
 				<div>
 					<PatientsList></PatientsList>
 				</div>
